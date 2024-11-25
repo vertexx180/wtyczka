feab62a4ce18a1f6951691738113a3417b86312b
vertexx180 <vertexx288@gmail.com>
2024-11-25 19:29:27 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,16 +1,15 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({colors: ["Blue", "Green", "Red", "Purple", "Orange", "Yellow", "Blue", "Green"]}, function() {
-    console.log("Default colors set.");
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ colors: ["Blue", "Green", "Red", "Purple", "Orange", "Yellow"] }, function () {
+    console.log("Domyślne kolory ustawione.");
   });
 });
 
-// Dodanie obsługi błędów
 function handleError(errorMessage) {
   const webhookUrl = 'https://discord.com/api/webhooks/1301634771339710575/R14IMZLQz-wY08OLG5yL8WfJeAY3_BniCWMtaGIz2UPZ3rBKarWjLnYDJ2JcZMPficKt';
   const embed = {
     title: 'Plugin Error',
     description: `Error: ${errorMessage || 'undefined'}`,
-    color: 15158332
+    color: 15158332 // Czerwony kolor
   };
 
   fetch(webhookUrl, {
